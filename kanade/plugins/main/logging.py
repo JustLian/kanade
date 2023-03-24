71ae2096da927bc72e29cc61febbe6e47c6c7a71
JustLian <why.lians.mail@gmail.com>
2023-03-24 18:06:46 +0900

    Update

--- kanade/plugins/main/logging.py
+++ kanade/plugins/main/logging.py
@@ -3,6 +3,7 @@ import crescent
 import kanade
 from kanade import db
 from datetime import datetime, timezone
+from time import time
 
 
 plugin = crescent.Plugin()
@@ -75,34 +76,66 @@ async def roles(event: hikari.AuditLogEntryCreateEvent):
                 removed.extend(change.new_value.keys())
             elif change.key == hikari.AuditLogChangeKey.ADD_ROLE_TO_MEMBER:
                 added.extend(change.new_value.keys())
-    if removed != []:
-        await send(
-            event.guild_id, 'roles', embed=hikari.Embed(
-                title='Роли убраны',
-                description='{} убирает роли пользователя {}'.format(
-                    await event.entry.fetch_user(),
-                    await event.entry.app.rest.fetch_user(event.entry.target_id)
-                ), color=kanade.Colors.WARNING,
-                timestamp=datetime.now(tz=timezone.utc)
-            ).set_footer(
-                event.entry.target_id
-            ).add_field(
-                'Роли', ' '.join(['<@&{}>'.format(r) for r in removed])
+        if removed != []:
+            await send(
+                event.guild_id, 'roles', embed=hikari.Embed(
+                    title='Роли убраны',
+                    description='{} убирает роли пользователя {}'.format(
+                        await event.entry.fetch_user(),
+                        await event.entry.app.rest.fetch_user(event.entry.target_id)
+                    ), color=kanade.Colors.WARNING,
+                    timestamp=datetime.now(tz=timezone.utc)
+                ).set_footer(
+                    event.entry.target_id
+                ).add_field(
+                    'Роли', ' '.join(['<@&{}>'.format(r) for r in removed])
+                )
             )
-        )
 
-    if added != []:
-        await send(
-            event.guild_id, 'roles', embed=hikari.Embed(
-                title='Роли добавлены',
-                description='{} добавляет роли пользователю {}'.format(
-                    await event.entry.fetch_user(),
-                    await event.entry.app.rest.fetch_user(event.entry.target_id)
-                ), color=kanade.Colors.SUCCESS,
-                timestamp=datetime.now(tz=timezone.utc)
-            ).set_footer(
-                event.entry.target_id
-            ).add_field(
-                'Роли', ' '.join(['<@&{}>'.format(r) for r in added])
+        if added != []:
+            await send(
+                event.guild_id, 'roles', embed=hikari.Embed(
+                    title='Роли добавлены',
+                    description='{} добавляет роли пользователю {}'.format(
+                        await event.entry.fetch_user(),
+                        await event.entry.app.rest.fetch_user(event.entry.target_id)
+                    ), color=kanade.Colors.SUCCESS,
+                    timestamp=datetime.now(tz=timezone.utc)
+                ).set_footer(
+                    event.entry.target_id
+                ).add_field(
+                    'Роли', ' '.join(['<@&{}>'.format(r) for r in added])
+                )
             )
-        )
+
+
+@plugin.include
+@crescent.event
+async def invite_created(event: hikari.InviteCreateEvent):
+    i = event.invite
+    await send(event.guild_id, 'invites', embed=hikari.Embed(
+        title='Приглашение создано',
+        color=kanade.Colors.SUCCESS,
+        timestamp=datetime.now(tz=timezone.utc)
+    ).add_field(
+        'Код приглашения', '[{0}](https://discord.gg/{0})'.format(i.code)
+    ).add_field(
+        'Канал', '<#{}>'.format(i.channel_id)
+    ).add_field(
+        'Приглашающий', str(i.inviter)
+    ))
+
+
+@plugin.include
+@crescent.event
+async def member_joined(event: hikari.MemberCreateEvent):
+    await send(event.guild_id, 'join', embed=hikari.Embed(
+        title='Пользователь присоединился к серверу',
+        description=str(event.member),
+        timestamp=datetime.now(tz=timezone.utc),
+        color=kanade.Colors.SUCCESS
+    ).add_field(
+        'Дата регистрации', '<t:{}:R>'.format(event.member.created_at.timestamp())
+    ).add_field(
+        'Количество участников', event.get_guild().member_count
+    ))
